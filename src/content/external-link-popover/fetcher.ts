ba10b87dc00f6849ae570540c3b4fd471197d05a
pythagoras-yamamoto <yamamo8533@gmail.com>
2025-02-14 11:30:46 +0900

    不要な箇所を削除

--- src/content/external-link-popover/fetcher.ts
+++ src/content/external-link-popover/fetcher.ts
@@ -7,27 +7,24 @@ export async function fetcher(lawIdOrNumOrRevisionId: string, elm?: string) {
     lawIdOrNumOrRevisionId
   )}`;
 
-  // クエリパラメータ指定
   const url = new URL(endpoint);
   // レスポンス: JSON
   url.searchParams.set("response_format", "json");
   // law_full_text: JSON形式
   url.searchParams.set("law_full_text_format", "json");
+
   if (elm) {
     url.searchParams.set("elm", elm);
   }
 
   try {
     const response = await fetch(url.toString());
-    console.log("APIリクエスト:", url.toString());
     if (!response.ok) {
       throw new Error("法令データの取得に失敗しました");
     }
     const json = await response.json();
-    console.log("APIレスポンス:", json);
     return json;
   } catch (error) {
-    console.error("法令APIの取得エラー:", error);
     throw error;
   }
 }
